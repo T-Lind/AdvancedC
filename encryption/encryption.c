827771762436e4dec5bdbd282afe8d0edcc0c89e
T-Lind <tiernanxklindauer@gmail.com>
2022-11-24 12:41:29 -0600

    successful encryption.c working!

--- encryption/encryption.c
+++ encryption/encryption.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
-#include "ReadFileLength.h"
 #include <string.h>
+#include <math.h>
 
-int encode(double rate, short index) {
-    double value = 0.5;
+
+long referenceIndex(int i) {
+    return 3.85 * i + 3 * log(i) + 13;
+}
+
+int encode(double rate, long index, double startValue) {
+    double value = startValue;
     for (int i = 0; i < index; i++)
         value = rate * value * (1 - value);
     return 100000 * value;
 }
 
 int main() {
-    char data[] = "owo!";
+    double startValue = 1 - sin(sqrt(sqrt(M_E)));
 
-    int fileLen = strlen(data);
+
+    char input[] = "The secret password is XA^40956123!\nDo not share this with anyone, except your homie Joe.";
+    int fileLen = strlen(input) + 1;
+
+    char data[fileLen];
+    data[0] = ' ';
+    for (int i = 1; i < fileLen; i++)
+        data[i] = input[i - 1];
 
     printf("Read data: ");
     char buffer[fileLen];
@@ -29,12 +41,12 @@ int main() {
     for (int i = 0; i < fileLen; i++) {
         short character = buffer[i];
         double rate = 3 + (double) character / 255;
-        int value = encode(rate, 10);
+        int value = encode(rate, referenceIndex(i), startValue);
         encryptBuffer[i] = value;
+//        printf("%f ", rate);
         printf("%d ", value);
     }
 
-    printf("\n\n");
 
     char decryptedData[fileLen];
     for (int i = 0; i < fileLen; i++) {
@@ -42,21 +54,21 @@ int main() {
 
         double rate = 0;
         for (double rateGuess = 3; rateGuess <= 4; rateGuess += (1 / 255.0)) {
-            int attempt = encode(rateGuess, 10);
+            int attempt = encode(rateGuess, referenceIndex(i), startValue);
             if (attempt == num) {
                 rate = rateGuess;
+                break;
             }
         }
-
-        if (rate != 0)
-            decryptedData[i] = 255.0 * (rate - 3);
-        else
-            printf(" ERROR ");
+        int decryptedVal = 255.0 * (rate - 3) + 1;
+        decryptedData[i] = decryptedVal;
     }
 
-    printf("Read data: ");
-    for (int i = 0; i < fileLen; i++) {
-        printf("%c ", decryptedData[i]);
+    printf("\n\n");
+
+    printf("Decrypted data: ");
+    for (int i = 1; i < fileLen; i++) {
+        printf("\b%c ", decryptedData[i]);
     }
 
     scanf("%hd", (short *) (0));
